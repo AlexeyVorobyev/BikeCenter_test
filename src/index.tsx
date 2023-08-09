e374d671298c068a05c40280368b22d6195be2e9
Воробьев Алексей Дмитриевич <42997441+AlexeyVorobyev@users.noreply.github.com>
2023-08-09 23:59:49 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import './index.scss'
+import App from './App';
+import {Provider} from "react-redux";
+import {store} from "./redux/store/store";
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
+
+
