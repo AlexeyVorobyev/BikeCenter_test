fa7cc12919d1aad8ae99d83029cab6399e9a471b
miste <mister.alex49@yandex.ru>
2023-08-12 00:46:51 +0300

    finished help page, dropdown in search

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
 import {Navigation} from "./components/Navigation/Navigation";
+import {Help} from "./components/Help/Help";
 
 function App() {
   return (
     <Routes>
-      <Route path={"/"} element={<Navigation/>}>
-        <Route index/>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Help/>}/>
       </Route>
     </Routes>
   );
